acd5661b100c0ba83129bd9323c5b56e6c561a0d
ADorian94 <igigaberos@gmail.com>
2023-05-02 18:19:08 +0200

    Add project files.

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BinarySearch;
+
+Console.WriteLine(SearchEngine.BinarySearch(new int[] { 2, 3, 5, 6, 8, 10, 12, 15, 19, 21 }, 6));
---
+++ BinarySearch/SearchEngine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    internal static class SearchEngine
+    {
+        public static int BinarySearch(int[] numbers, int target)
+        {
+            int low = 0, high = numbers.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+
+                if (numbers[mid] == target)
+                    return mid;
+                else if (target < numbers[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+
+            return -1; //target doesn't exist int the array
+        }
+    }
+}
